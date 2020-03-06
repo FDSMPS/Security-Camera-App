763ebb888a4419a3844469303239abc98633f8b6
Tymoore Jamal <tymoore@ualberta.ca>
2020-03-06 15:45:39 -0700

    commented some lines

--- src/ServoMotor.py
+++ src/ServoMotor.py
@@ -16,6 +16,8 @@ class ServoMotorInterface():
             Sets the servo's X angle
             @param angle: the angle to set
         '''
+        # self.start_servo_x()
+
         if angle > self.settings["ServoMotorXMax"]:
             angle = self.settings["ServoMotorXMax"]
 
@@ -23,17 +25,39 @@ class ServoMotorInterface():
             angle = self.settings["ServoMotorXMin"]
         self.xpwm.ChangeDutyCycle(angle)
 
+        # self.stop_servo_x()
+
+
     def set_servo_y_angle(self, angle):
         '''
             Sets the servo's Y angle
             @param angle: the angle to set
         '''
+
+        # self.start_servo_y()
+
         if angle > self.settings["ServoMotorYMax"]:
             angle = self.settings["ServoMotorYMax"]
         
         if angle < self.settings["ServoMotorYMin"]:
             angle = self.settings["ServoMotorYMin"]
         self.ypwm.ChangeDutyCycle(angle)
+
+        # self.stop_servo_y()
+
+
+    def start_servo_x(self):
+        self.xpwm.start(0)
+
+    def start_servo_y(self):
+        self.ypwm.start(0)
+
+    def stop_servo_x(self):
+        self.xpwm.stop()
+
+    def stop_servo_y(self):
+        self.ypwm.stop()
+
 
     def __init__(self, settings):
         '''
@@ -51,7 +75,7 @@ class ServoMotorInterface():
         self.xpwm = GPIO.PWM(self.settings["ServoMotorXPin"], 50)
         self.ypwm = GPIO.PWM(self.settings["ServoMotorYPin"], 50)
 
-        self.xpwm.start(0)
-        self.ypwm.start(0)
+        self.start_servo_x()
+        self.start_servo_y()
 
 
